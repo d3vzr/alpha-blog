e83d6f282ca51a02de2783ab9b791cf813fd05c9
Ahmed Abdelsalam <btbe4ever@gmail.com>
2016-07-10 01:11:04 +0000

    Create articles table , model and add validation

---
+++ db/migrate/20160709194215_add_desciption_to_articles.rb
@@ -0,0 +1,8 @@
+class AddDesciptionToArticles < ActiveRecord::Migration
+  def change
+    add_column :articles, :description, :text
+    add_column :articles, :created_at, :datetime
+    add_column :articles, :updated_at, :datetime
+
+  end
+end
