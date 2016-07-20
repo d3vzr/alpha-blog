587df68099ed2b5f0a0a45132f683cd2a2eb15d6
Ahmed Abdelsalam <btbe4ever@gmail.com>
2016-07-20 12:30:54 +0000

    add pagination to some pages

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
   belongs_to :user
-  validates :title, presence: true , length: { minimum:3, maximum:50 }
-  validates :description, presence: true , length: { minimum:10, maximum:300 }
+  validates :title, presence: true , length: { minimum:3, maximum:100 }
+  validates :description, presence: true , length: { minimum:10, maximum:1000 }
   validates :user_id, presence: true
 end
