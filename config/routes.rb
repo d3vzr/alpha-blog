777842e5299afcdfc52c849870377f1c878239d6
Ahmed Abdelsalam <btbe4ever@gmail.com>
2016-07-20 10:43:39 +0000

    create show actionn and gravatar image

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
 
    resources :articles
 
+  get 'signup', to:"users#new"
+#   post 'users', to: 'users#create'
+  resources :users, except: [:new]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
