842fb05d17a714c516c8804f8319277de0d27b1f
Ahmed Abdelsalam <btbe4ever@gmail.com>
2016-07-28 13:01:13 +0000

    add article and category association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   validates :title, presence: true , length: { minimum:3, maximum:100 }
   validates :description, presence: true , length: { minimum:10, maximum:1000 }
   validates :user_id, presence: true
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,7 @@ class CategoriesControllerTest < ActionController::TestCase
 
   def setup
     @category = Category.create(name: "sports")
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
   end
 
   test "should get categores index" do
@@ -12,7 +13,7 @@ class CategoriesControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
-
+    session[:user_id] = @user.id
     get :new
     assert_response :success
 
@@ -21,6 +22,13 @@ class CategoriesControllerTest < ActionController::TestCase
   test "shoud get show" do
     get( :show, {'id' =>@category.id})
     assert_response :success
+  end
+
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post :create, category: { name: "sports" }
+    end
+    assert_redirected_to categories_path
   end
 
 end
